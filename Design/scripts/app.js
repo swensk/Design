6e688739da2d421be2a96a0bc8bd87242f370995
Johannes Mueller <m_johannes@hotmail.com>
2014-03-28 19:35:20 -0500

    Changes made by Johannes Mueller (m_johannes@hotmail.com).

--- Design/scripts/app.js
+++ Design/scripts/app.js
@@ -1,12 +1,14 @@
 (function (global) {
     var mobileSkin = "",
         app = global.app = global.app || {};
+    
 
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
     }, false);
 
     app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout"});
+    
 
     app.changeSkin = function (e) {
         if (e.sender.element.text() === "Flat") {
