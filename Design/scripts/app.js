b15f5211cfdd6ab89aa27235e1e394dbe66021f9
Conny Burman <Curre_@outlook.com>
2014-03-31 15:59:23 -0500

    Changes made by Conny Burman (Curre_@outlook.com).

--- Design/scripts/app.js
+++ Design/scripts/app.js
@@ -9,17 +9,6 @@
 
     app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout"});
     
-
-    app.changeSkin = function (e) {
-        if (e.sender.element.text() === "Flat") {
-            e.sender.element.text("Native");
-            mobileSkin = "flat";
-        }
-        else {
-            e.sender.element.text("Flat");
-            mobileSkin = "";
-        }
-
-        app.application.skin(mobileSkin);
-    };
+	app.application.skin("Flat");
+    
 })(window);
